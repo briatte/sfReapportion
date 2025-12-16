442a8c7b9db7beb809a23d6d8bcee5f8e6dee7e0
François Briatte <briatte@gmail.com>
2025-12-16 12:39:26 +0100

    additional tests

--- tests/testthat/test.spReapportion.R
+++ tests/testthat/test.spReapportion.R
@@ -1,6 +1,5 @@
 library(sfReapportion)
 
-
 test_that("sfReapportion reapportions data correctly", {
   skip_on_cran()
   data(ParisPollingStations2012)
@@ -15,9 +14,45 @@ test_that("sfReapportion reapportions data correctly", {
                             update = FALSE)
 })
 
+test_that("sfReapportion works with `sf` objects", {
+  skip_on_cran()
+  data(ParisPollingStations2012)
+  data(ParisIris)
+  data(RP_2011_CS8_Paris)
+
+  # convert both geoms to `sf`
+  ParisPollingStations2012 <- sf::st_as_sf(ParisPollingStations2012)
+  ParisIris <- sf::st_as_sf(ParisIris)
+
+  CS_ParisPollingStations <- sfReapportion(ParisIris,
+                                           ParisPollingStations2012,
+                                           RP_2011_CS8_Paris,
+                                           "DCOMIRIS", "ID", "IRIS")
+  expect_equal_to_reference(CS_ParisPollingStations,
+                            "CS_ParisPollingStations.rds",
+                            update = FALSE)
+})
+
+test_that("sfReapportion can merge polygons", {
+  skip_on_cran()
+  data(ParisPollingStations2012)
+  data(ParisIris)
+  data(RP_2011_CS8_Paris)
+
+  # create duplicated IDs
+  x <- seq(42, 42 + 150, by = 2)
+  ParisIris@data[["DCOMIRIS"]][ x ] <- ParisIris@data[["DCOMIRIS"]][ x + 1 ]
+
+  testthat::expect_message(sfReapportion::sfReapportion(ParisIris,
+                                                        ParisPollingStations2012,
+                                                        RP_2011_CS8_Paris,
+                                                        "DCOMIRIS", "ID", "IRIS"),
+                           "^Merging 76 SpatialPolygons with duplicate IDs")
+})
+
 ###
-### this test omits to pass "mode = 'proportion'", which is the only reason
-### why my current code passes it successfully...
+### this test did not work in {sfReapportion} due to the omission of
+### `mode = "proportion` -- the reference data was re-saved in v0.1.3
 ###
 test_that("sfReapportion reapportions data correctly with proportions data", {
   skip_on_cran()
@@ -36,7 +71,6 @@ test_that("sfReapportion reapportions data correctly with proportions data", {
                             "CS_ParisPollingStationsprop.rds",
                             update = FALSE)
 })
-
 
 test_that("NA values are handled correctly", {
   data(ParisPollingStations2012)
