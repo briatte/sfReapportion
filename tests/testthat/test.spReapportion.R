54a3315af78948d6a542419dacc63c8306653991
François Briatte <briatte@gmail.com>
2025-12-16 11:27:59 +0100

    cosmetics

--- tests/testthat/test.spReapportion.R
+++ tests/testthat/test.spReapportion.R
@@ -6,8 +6,13 @@ test_that("sfReapportion reapportions data correctly", {
   data(ParisPollingStations2012)
   data(ParisIris)
   data(RP_2011_CS8_Paris)
-  CS_ParisPollingStations <- sfReapportion(ParisIris, ParisPollingStations2012, RP_2011_CS8_Paris, "DCOMIRIS", "ID", "IRIS")
-  expect_equal_to_reference(CS_ParisPollingStations, "CS_ParisPollingStations.rds")
+  CS_ParisPollingStations <- sfReapportion(ParisIris,
+                                           ParisPollingStations2012,
+                                           RP_2011_CS8_Paris,
+                                           "DCOMIRIS", "ID", "IRIS")
+  expect_equal_to_reference(CS_ParisPollingStations,
+                            "CS_ParisPollingStations.rds",
+                            update = FALSE)
 })
 
 ###
